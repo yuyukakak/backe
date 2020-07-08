ba0e3785dd19b9027e407fb13f7df2fe87b6affa
Fabio Ribeiro Martins <fabio.ribeiro-martins@live.com>
2020-07-08 19:55:32 -0300

    Refresh token implemented

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const { Account } = require('../models');
 const { accountSignUp, accountSignIn } = require('../validators/account');
 const { getMessage } = require('../helpers/messages');
-const { generateJwt, generateRefreshJwt } = require('../helpers/jwt');
+const { generateJwt, generateRefreshJwt, getTokenFromHeaders, verifyRefreshJwt, verifyJwt } = require('../helpers/jwt');
 
 const router = express.Router();
 const saltOrRound = 10;
@@ -15,12 +15,12 @@ router.post('/sign-in', accountSignIn, async (req, res) => {
 
     //Validate password
     const match = account ? bcrypt.compareSync(password, account.password) : null;
-    if(!match) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
+    if (!match) return res.jsonBadRequest(null, getMessage('account.signin.invalid'));
 
     const token = generateJwt({ id: account.id });
     const refreshToken = generateRefreshJwt({ id: account.id, version: account.jwtVersion });
 
-    return res.jsonOk(account, getMessage('account.signin.success'), {token, refreshToken});
+    return res.jsonOk(account, getMessage('account.signin.success'), { token, refreshToken });
 });
 
 router.post('/sign-up', accountSignUp, async (req, res) => {
@@ -34,10 +34,36 @@ router.post('/sign-up', accountSignUp, async (req, res) => {
     const hash = bcrypt.hashSync(password, saltOrRound);
     const newAccount = await Account.create({ email, password: hash });
 
-    const token = generateJwt({ id: account.id });
-    const refreshToken = generateRefreshJwt({ id: account.id, version: newAccount.jwtVersion });
+    const token = generateJwt({ id: newAccount.id });
+    const refreshToken = generateRefreshJwt({ id: newAccount.id, version: newAccount.jwtVersion });
 
     return res.jsonOk(newAccount, getMessage('account.signup.success'), { token, refreshToken });
+});
+
+router.post('/refresh', async (req, res) => {
+    const token = getTokenFromHeaders(req.headers);
+    if (!token) return res.jsonUnauthorized(null, 'Invalid token');
+
+    try {
+        const decoded = verifyRefreshJwt(token);
+        const account = await Account.findByPk(decoded.id);
+
+        if (!account) return res.jsonUnauthorized(null, 'Invalid token');
+
+        if (decoded.version !== account.jwtVersion) {
+            return res.jsonUnauthorized(null, 'Invalid token');
+        }
+
+        const meta = {
+            token: generateJwt({ id: account.id })
+        }
+
+        return res.jsonOk(null, null, meta);
+
+    } catch (error) {
+        return res.jsonUnauthorized(null, error.message);
+    }
+
 });
 
 module.exports = router;
